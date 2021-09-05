aaae566591e000afa3488fda90205dbc04e63616
Shvyryov AN <artem.shv9@yandex.ru>
2021-09-05 18:37:35 +0300

    add user methods

--- bot/database/models.py
+++ bot/database/models.py
@@ -8,9 +8,9 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
-    full_name = Column(String(255))
-    phone_number = Column(String(25))
+    id = Column(Integer, primary_key=True, doc="Идентификатор")
+    full_name = Column(String(255), doc="ФИО")
+    phone_number = Column(String(25), doc="Номер телефона")
 
     order = relationship("Order")
 
@@ -18,29 +18,29 @@ class User(Base):
 class Order(Base):
     __tablename__ = 'order'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    price = Column(Integer)
-    delivery_id = Column(Integer)
-    address_to = Column(String(1000))
+    id = Column(Integer, primary_key=True, autoincrement=True, doc="Идентификатор")
+    price = Column(Integer, doc="Итоговая цена")
+    delivery_id = Column(Integer, doc="Способ доставки")
+    address_to = Column(String(1000), doc="Адрес доставки")
+    user_id = Column(Integer, ForeignKey('user.id'), unique=True, doc="Идентификатор пользователя")
 
     ordered_product = relationship("OrderedProduct")
 
-    user_id = Column(Integer, ForeignKey('user.id'), unique=True)
 
 
 class OrderedProduct(Base):
     __tablename__ = 'ordered_product'
 
-    order_id = Column(Integer, ForeignKey('order.id'), primary_key=True)
-    product_id = Column(Integer, ForeignKey('product.id'), primary_key=True)
-    count = Column(Integer)
+    order_id = Column(Integer, ForeignKey('order.id'), primary_key=True, doc="Идентификатор заказа")
+    product_id = Column(Integer, ForeignKey('product.id'), primary_key=True, doc="Идентификатор товара")
+    count = Column(Integer, doc="Количество")
 
 
 class Product(Base):
     __tablename__ = 'product'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255), unique=True)
-    price = Column(Integer)
+    id = Column(Integer, primary_key=True, doc="Идентификатор")
+    name = Column(String(255), unique=True, doc="Наименование")
+    price = Column(Integer, doc="Цена")
 
     ordered_product = relationship("OrderedProduct")
