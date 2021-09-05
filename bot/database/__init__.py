0ea1205ec47ae2ff24b0bb84c4f114deb5d1cc48
Shvyryov AN <artem.shv9@yandex.ru>
2021-09-05 14:46:49 +0300

    add user methods

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,1 +1,2 @@
 from bot.database.models import User, Basket, Product
+from bot.database.crud.users import update_user, create_user, delete_user
--- bot/database/crud/users.py
+++ bot/database/crud/users.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy import update
+from sqlalchemy import update, delete
 
 from bot.database.models import User
 from bot.database.engine import Session
@@ -20,5 +20,15 @@ def update_user(id: int, full_name: Optional[str] = None, phone_number: Optional
             update(User).
             where(User.id == id).
             values(full_name=full_name, phone_number=phone_number)
+        )
+        session.commit()
+
+
+def delete_user(id: int) -> None:
+    """Удалить пользователя из базы данных."""
+    with Session() as session:
+        session.execute(
+            delete(User).
+            where(User.id == id)
         )
         session.commit()
